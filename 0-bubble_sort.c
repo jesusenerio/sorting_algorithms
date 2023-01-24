9672dcedeb95c76e2182cea6a6b22d192e3fd7d5
jesusenerio <jesusenerio50@gmail.com>
2023-01-24 12:23:55 -0800

    c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 #include "sort.h"
 
+/**
+ * bubble_sort - this is the main bubble sort algorithm
+ * @array: this is the array to sort
+ * @size: this is the size of the array to sort
+ * i: an int to iterate
+ * j: an int to iterate
+ * temp: to assign new array
+ * Return: 0 always
+ */
+
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, temp;
-	if(size < 2)
+
+	if (size < 2)
 		return;
-	for(i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for(j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if(array[j] > array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
---
+++ README.md
@@ -0,0 +1,7 @@
+this is the sorting algorithm project
+we are working on it in team of 2
+here you will find the 
+*Bubble Sort Algorithm
+*Selection Sort Algprithm 
+*Insertion Sort Algorthm
+*Quick Sort Algorithm
