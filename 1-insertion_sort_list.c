c6c91ddb9e1d4e9c8098a41a15dc36ab0bd1bf6e
jesusenerio <jesusenerio50@gmail.com>
2023-01-26 02:17:18 -0800

    c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,38 +1,42 @@
-#include <stdio.h>
 #include "sort.h"
 
 /**
- * insertion_sort_list - this is the insertion algorithm
+ * insertion_sort_list - the insertion sort algorithm
  * @list: the list to sort
- * current: used for iterating
- * temp: used for swapping
+ * current: use to iterate
+ * temp: use for swapping
  * Return: 0 always
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *temp;
+	listint_t *current;
+	listint_t *temp;
 
-	if (list == NULL || *list == NULL)
+	if (*list == NULL || (*list)->next == NULL)
 		return;
 
 	current = (*list)->next;
 	while (current != NULL)
 	{
-		temp = current;
-		while (temp->prev != NULL && temp->n < temp->prev->n)
+		temp = current->prev;
+		while (temp != NULL && temp->n > current->n)
 		{
-			temp->prev->next = temp->next;
-			if (temp->next != NULL)
-				temp->next->prev = temp->prev;
-			temp->prev = temp->prev->prev;
-			temp->next = temp->prev->next;
-			temp->prev->next = temp;
-			if (temp->next != NULL)
-				temp->next->prev = temp;
+			/* Swap the current and prev nodes */
+			current->prev = temp->prev;
+			if (temp->prev != NULL)
+				temp->prev->next = current;
+			temp->prev = current;
+			temp->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = temp;
+			current->next = temp;
+			/* Print the list */
 			print_list(*list);
-			temp = temp->prev;
+			temp = current->prev;
 		}
+		if (current->prev == NULL)
+			*list = current;
 		current = current->next;
 	}
 }
